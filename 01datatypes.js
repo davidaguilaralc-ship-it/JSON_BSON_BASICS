b1773491ac57b34591b6e44328733d7251d8fe27
davidaguilaralc-ship-it <david.aguilar.alc@cbtis258.edu.mx>
2026-02-17 11:39:47 -0600

    subida de los primeros archivos

---
+++ 01datatypes.js
@@ -0,0 +1,16 @@
+const planeta={
+    "Planeta":"Jupiter",
+    "tipo":"Gaseoso",
+    "Estrella":"Sol",
+    "color":"naranja/blanco",
+    "Gravedad":"24,79 m/s²",
+}
+console.log("---Documento Completo---")
+console.table(planeta)
+console.log("---Datos especificos---")
+console.log("El nombre es:", planeta.Planeta)
+console.log("Tipo:", planeta.tipo)
+console.log("Estrella:", planeta.Estrella)
+console.log("Color:", planeta.color)
+console.log("Gravedad:", planeta.Gravedad)
+
